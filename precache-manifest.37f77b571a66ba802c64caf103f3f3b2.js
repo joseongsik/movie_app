2a4812e1daaa5bdb7744106960407808d4cc4bb3
Joseongsik <ss.jo@4cgate.com>
2019-06-12 19:30:28 +0900

    Updates

---
+++ precache-manifest.37f77b571a66ba802c64caf103f3f3b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81797849a075cdf77544e15618c804d4",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "7101f5a30c6c8518f49c",
+    "url": "/movie_app/static/css/main.705e7b6b.chunk.css"
+  },
+  {
+    "revision": "5cdb4062c6dfdd077298",
+    "url": "/movie_app/static/js/2.31ebf22a.chunk.js"
+  },
+  {
+    "revision": "7101f5a30c6c8518f49c",
+    "url": "/movie_app/static/js/main.92cf58a7.chunk.js"
+  },
+  {
+    "revision": "c6514544d436775abdc2",
+    "url": "/movie_app/static/js/runtime~main.175dda75.js"
+  }
+]);
